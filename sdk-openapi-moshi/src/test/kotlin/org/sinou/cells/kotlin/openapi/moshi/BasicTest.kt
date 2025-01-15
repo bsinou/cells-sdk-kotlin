cbf52ba2d3aef0bc9aa9dc221998d583b3854a93
Bruno Sinou <bruno.sinou@posteo.de>
2025-01-15 22:05:20 +0100

    A few more tests

--- sdk-openapi-moshi/src/test/kotlin/org/sinou/cells/kotlin/openapi/moshi/BasicTest.kt
+++ sdk-openapi-moshi/src/test/kotlin/org/sinou/cells/kotlin/openapi/moshi/BasicTest.kt
@@ -2,13 +2,17 @@
 
 package org.sinou.cells.kotlin.openapi.moshi
 
+import io.kotlintest.matchers.numerics.shouldBeGreaterThanOrEqual
 import io.kotlintest.shouldBe
+import io.kotlintest.shouldNotBe
 import io.kotlintest.specs.ShouldSpec
 import org.sinou.cells.kotlin.openapi.moshi.api.NodeServiceApi
 import java.util.UUID
 import org.sinou.cells.kotlin.openapi.moshi.model.RestCreateRequest
 import org.sinou.cells.kotlin.openapi.moshi.model.RestIncomingNode
+import org.sinou.cells.kotlin.openapi.moshi.model.RestLookupRequest
 import org.sinou.cells.kotlin.openapi.moshi.model.RestNodeLocator
+import org.sinou.cells.kotlin.openapi.moshi.model.RestNodeLocators
 import org.sinou.cells.kotlin.openapi.moshi.model.TreeNodeType
 
 class BasicTest : ShouldSpec() {
@@ -19,23 +23,61 @@ class BasicTest : ShouldSpec() {
         should("ping server") { pingServer() shouldBe 200 }
 
         should("test create") {
-            val id: String = UUID.randomUUID().toString()
-            val newPath = "common-files/test-kotlin-moshi-$id.txt"
+            val name = "test-kotlin-moshi-${Unique()}.txt"
+
+            val newPath = "common-files/$name"
             try {
-                val currRequest =
-                    RestCreateRequest(
-                        inputs =
-                        listOf(
+                val q1 = RestCreateRequest(
+                        inputs = listOf(
                             RestIncomingNode(
                                 type = TreeNodeType.LEAF,
                                 locator = RestNodeLocator(path = newPath),
                                 contentType = "text/plain")),
-                        recursive = false)
-                val response = apiInstance.create(currRequest)
-                print("Found ${response.nodes.size} nodes")
+                        recursive = false,
+                    )
+                val r1 = apiInstance.create(q1)
+                r1.nodes.size shouldBe 1
+
+                val q2 = RestLookupRequest(
+                    locators = RestNodeLocators(listOf(RestNodeLocator(path = newPath))),
+                )
+                val r2 = apiInstance.lookup(q2)
+                r2.nodes.size shouldBeGreaterThanOrEqual 1
+                var found = false
+                var  foundUuid : String? = null
+                r2.nodes.forEach({ if (it.path == newPath) {
+                    if (it.uuid == r1.nodes[0].uuid) {
+                        found = true
+                        foundUuid = it.uuid
+                    }
+                } })
+                found shouldBe true
+                foundUuid shouldNotBe null
+
+                val q3 = RestLookupRequest(locators = RestNodeLocators(listOf(
+                    RestNodeLocator(uuid = foundUuid, path = "common-files/*")
+                )))
+                val r3 = apiInstance.lookup(q3)
+                r3.nodes.size shouldBe 1
+//
+//
+//                val r4 = apiInstance.performAction(
+//                    name = NamePerformAction.delete,
+//                    parameters = RestActionParameters (
+//                        nodes = listOf(RestNodeLocator(uuid = r1.nodes[0].uuid))
+//                    )
+//                )
+//                r4.status shouldBe RestActionStatus.Background
+
             } catch (e: Exception) {
                 e shouldBe null
             }
         }
     }
+}
+
+
+fun Unique(lenght: Int = 4) : String {
+    val id: String = UUID.randomUUID().toString()
+    return id.take(lenght)
 }
